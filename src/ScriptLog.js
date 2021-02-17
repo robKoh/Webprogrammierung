953c8bcfef84e56e54a515459913cf0d23ce6a0e
Robi12689 <robertkohn00@gmail.com>
2021-02-17 18:18:20 +0100

    Kleinigkeit#2

--- src/ScriptLog.js
+++ src/ScriptLog.js
@@ -9,33 +9,33 @@ function submitTextChange() {
 // Liste-Bereich
 var count = 0;
 
-        function neuerBegriff() {
-            var inputWord = document.getElementById("begriffe").value;
-            if (count == 0) {
-                if (inputWord != "") {
-                    var ulElement = document.createElement("ul");
-                    var liElement = document.createElement("li");
-                    var listeBereich = document.getElementById("liste");
-                    liElement.innerText = inputWord;
-                    ulElement.appendChild(liElement);
-                    listeBereich.appendChild(ulElement);
-                }
-            } else {
-                if (inputWord != "") {
-                    var liElement = document.createElement("li");
-                    var listeBereich = document.getElementById("liste");
-                    var ulElementCollection = listeBereich.getElementsByTagName("ul");
-                    var ulElement = ulElementCollection[ulElementCollection.length - 1]
-                    liElement.innerText = inputWord;
-                    ulElement.appendChild(liElement);
-                }
-            }
-            count++;
-        }
-
-        function deleteBegriff() {
+function neuerBegriff() {
+    var inputWord = document.getElementById("begriffe").value;
+    if (count == 0) {
+        if (inputWord != "") {
+            var ulElement = document.createElement("ul");
+            var liElement = document.createElement("li");
             var listeBereich = document.getElementById("liste");
-            var liArray = listeBereich.getElementsByTagName("li");
-            liArray[liArray.length - 1].remove();
-            liArray[liArray.length - 1] = null;
+            liElement.innerText = inputWord;
+            ulElement.appendChild(liElement);
+            listeBereich.appendChild(ulElement);
         }
+    } else {
+        if (inputWord != "") {
+            var liElement = document.createElement("li");
+            var listeBereich = document.getElementById("liste");
+            var ulElementCollection = listeBereich.getElementsByTagName("ul");
+            var ulElement = ulElementCollection[ulElementCollection.length - 1]
+            liElement.innerText = inputWord;
+            ulElement.appendChild(liElement);
+        }
+    }
+    count++;
+}
+
+function deleteBegriff() {
+    var listeBereich = document.getElementById("liste");
+    var liArray = listeBereich.getElementsByTagName("li");
+    liArray[liArray.length - 1].remove();
+    liArray[liArray.length - 1] = null;
+}
