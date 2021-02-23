1bb344f6fd6e8e0e8832e14a63b824e76ac468db
Robi12689 <robertkohn00@gmail.com>
2021-02-23 15:45:54 +0100

    server

--- server.js
+++ server.js
@@ -16,27 +16,6 @@ const users = new Map();
 // add static login page
 app.use("/", express.static(__dirname + "/index.html"));
 
-//app.get("/", (req, res) => {
-  //var i = 0;
-  //id = uuidv4();
-  //users.set(i, id);
-  /*res.cookie("test test", {
-    expire: 3600000 + Date.now(),
-    httpOnly: true,
-    secure: true
-  */  
- // res.sendFile("/index.html");
-    
-//});
-//});
-
-/*app.post("/login", (req, res) => {
-  const user = req.body;
-  users[uuidv4()] = user;
- // res.redirect("src/comment.html");
-  res.statusCode = 201;
-  res.send();
-});*/
 
 app.listen(PORT, () => {
   console.log(`This app listening at http://localhost:${PORT}`);
