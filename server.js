5788c2be034387cb37fadd21c7a05f88396c35f2
Robi12689 <robertkohn00@gmail.com>
2021-02-24 16:27:06 +0100

    Logout Endpunkt und CookieSessions integriert, sodass im Cookie-Fenster unter username = "meyer" bspw. steht

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
-const cookieParser = require("cookie-parser");
+//const cookieParser = require("cookie-parser");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
 const PORT = 8080;
 
 // App
-app.use(bodyParser.json());
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("src"));
+
+app.use(
+  cookieSession({
+    name: "session",
+    signed: false,
+    expires: new Date(Date.now() + 3600000)
+  })
+);
 
 const users = new Map();
 
 app.post('/', (req, res) => {
-  const user = req.body;
+  req.session.username = req.body.username;
+  const user = req.body.username;
   users[uuidv4()] = user;
   res.redirect("/comment.html");
   res.statusCode = 201;
+  res.send();
+});
+
+app.get("/logout", (req, res) => {
+  req.session = null;
+  res.redirect("/");
   res.send();
 });
 
