5f144b89d9b7d2333f905b1b212dec919ab93988
NiklasZinngrebe <nz@niklaszinngrebe.de>
2021-02-24 15:40:38 +0100

    fixed dockerfile, fixed comment.html redirect

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,32 @@
+#FROM node:14
+
+# Create app directory
+#WORKDIR /app
+
+#RUN git clone https://github.com/robKoh/Webprogrammierung.git .
+
+#RUN npm install
+
+#EXPOSE 8080
+#CMD [ "node", "server.js" ]
+
 FROM node:14
 
 # Create app directory
-WORKDIR /app
+WORKDIR /usr/src/app
 
-RUN git clone https://github.com/robKoh/Webprogrammierung.git .
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package*.json ./
 
 #RUN npm ci
 RUN npm install && npm ci
+# If you are building your code for production
+# RUN npm ci --only=production
+
+# Bundle app source
+COPY . .
 
 EXPOSE 8080
-CMD [ "node", "server.js" ]
+CMD [ "node", "server.js" ] 
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const cookieParser = require("cookie-parser");
@@ -7,15 +7,19 @@ const app = express();
 const PORT = 8080;
 
 // App
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
-//app.use(express.static("src"));
+app.use(express.static("src"));
 
 const users = new Map();
 
-// add static login page
-app.use("/", express.static(__dirname + "/index.html"));
-
+app.post('/', (req, res) => {
+  const user = req.body;
+  users[uuidv4()] = user;
+  res.redirect("/comment.html");
+  res.statusCode = 201;
+  res.send();
+});
 
 app.listen(PORT, () => {
   console.log(`This app listening at http://localhost:${PORT}`);
