3bea8e549284efaf6923750f1d19a6af3696ef77
Robi12689 <robertkohn00@gmail.com>
2021-02-25 19:36:21 +0100

    Jetzt sollte der Benutzername angezeigt werden

--- src/displayUser.js
+++ src/displayUser.js
@@ -4,18 +4,17 @@ fetch("/displayUser")
   )
   .then((json) => {
     const spanElement = document.getElementById("display_user");
-
-    json.forEach((dataItem) => {
-      if (dataItem === null) {
-        alert("Benutzer konnte nicht angezeigt werden.");
-      } else {
-        var text = document.createTextNode(dataItem);
-        spanElement.appendChild(text);
-      }
-    });
-
-    if (json.length === 0) {
-        var text = document.createTextNode("Unbekannt");
-        spanElement.appendChild(text);
+    const jsonName = JSON.parse(json); 
+    
+    if (jsonName === "") {
+      alert("Benutzer konnte nicht angezeigt werden.");
+    } else {
+      var text = document.createTextNode(jsonName);
+      spanElement.appendChild(text);
     }
-});
+  });
+
+  if (json.length === 0) {
+      var text = document.createTextNode("Unbekannt");
+      spanElement.appendChild(text);
+  }
