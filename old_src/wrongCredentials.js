a485455667e5103439dc03ce8d3c3068a559dc99
Stresser200 <matze1199@web.de>
2021-03-11 11:41:48 +0100

    Merged

--- old_src/wrongCredentials.js
+++
@@ -1,8 +0,0 @@
-function validateInputForm() {
-  const inputUsername = document.getElementById("username");
-  const inputPassword = document.getElementById("password");
-
-  if (inputUsername.value === "" || inputPassword.value === "") {
-    alert("Sie müssen einen Benutzername und ein Passwort eingeben!");
-  }
-}
